a52297a6952ba2b8c28237fbc1aeacda64d03808
pacoDan1 <31734608+pacoDan1@users.noreply.github.com>
2018-05-21 17:19:42 -0300

    Add files via upload
    probandoxD

---
+++ src/test/java/categoria/ParaCategoria1.java
@@ -0,0 +1,14 @@
+package entrega0;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParaCategoria1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
